9e9a53cbf3f25b45f0fa2b5f772e9e9830bd71f6
Dorel Ben Hamo <dorel.bh1@gmail.com>
2021-12-13 18:22:57 +0200

    FAQ-Userr

---
+++ firstAPP/app/src/main/java/com/example/myfirstapp/FAQUser.java
@@ -0,0 +1,13 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FAQUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faquser);
+    }
+}
