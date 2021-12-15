700fbe2493d75110a6228dd755cfc392be8957c3
ronmoyal <ronmoyal1902@gmail.com>
2021-12-15 16:45:59 +0200

    change

--- firstAPP/app/src/main/java/com/example/myfirstapp/ClientScreenMain.java
+++ firstAPP/app/src/main/java/com/example/myfirstapp/ClientScreenMain.java
@@ -1,5 +1,6 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,13 +13,18 @@ import android.widget.TextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ClientScreenMain extends AppCompatActivity implements View.OnClickListener {
     private TextView welcome;
     private Button btnAdd;
     private Button logout;
-
     FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    DatabaseReference reference;
 
 
     @Override
@@ -30,12 +36,25 @@ public class ClientScreenMain extends AppCompatActivity implements View.OnClickL
         welcome=(TextView) findViewById(R.id.welcomeClient);
         btnAdd=(Button)findViewById(R.id.btn_Add);
         logout=(Button)findViewById(R.id.btnLogOutClient);
-        welcome.setText("welcome:"+"");
 
         firebaseAuth = FirebaseAuth.getInstance();
-
+        user = FirebaseAuth.getInstance().getCurrentUser();// this user
         btnAdd.setOnClickListener(this);
         logout.setOnClickListener(this);
+
+        reference= FirebaseDatabase.getInstance().getReference().child("Users").child(user.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name=snapshot.child("fullname").getValue().toString();
+                welcome.setText("Welcome: "+""+name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
