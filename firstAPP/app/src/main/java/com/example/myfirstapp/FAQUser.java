3f18afd4f131bbec40235a2c88c73b48b730f3a0
Dorel Ben Hamo <dorel.bh1@gmail.com>
2021-12-16 11:31:59 +0200

    faqquseer

--- firstAPP/app/src/main/java/com/example/myfirstapp/FAQUser.java
+++ firstAPP/app/src/main/java/com/example/myfirstapp/FAQUser.java
@@ -50,15 +50,65 @@ public class FAQUser extends AppCompatActivity implements View.OnClickListener {
         btnCatWash.setOnClickListener(this);
         btnCatMarch.setOnClickListener(this);
         btnCatEat.setOnClickListener(this);
-        btnDogEat.setOnClickListener(this);
         btnCatMilk.setOnClickListener(this);
+        btnCatPoop.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnDogEat:
-                Toast.makeText(this, "כל כלב הוא עולם ומלאו! חשוב לאבחן אם יש סיבה רגשית לעובדה שהוא לא אוכל. אם זה כלב חדש שהגיע הביתה אז אולי הוא עדיין בסטרס ומנסה להתאקלם. אולי יש לו מצב רוח ירוד בגלל איזשהו שינוי בבית, בשגרה וכו'... אני ממליצה להיעזר באיש מקצוע על מנת לנסות להגיע לסיבה לחוסר התיאבון של הכלב.", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "כל כלב הוא עולם ומלאו!\n" +
+                        "חשוב לאבחן אם יש סיבה רגשית לעובדה שהוא לא אוכל.\n" +
+                        "אם זה כלב חדש שהגיע הביתה אז אולי הוא עדיין בסטרס ומנסה להתאקלם אולי יש לו מצב רוח ירוד בגלל איזשהו שינוי בבית, בשגרה וכו'... \n" +
+                        "מומלץ להיעזר באיש מקצוע!\n", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnDogBurn:
+                Toast.makeText(this, "כטיפול ראשוני (אפילו בשטח) כדאי לחבוש את הרגל ולמנוע זיהום של המקום.\n" +
+                        "לאחר מכן כדאי להגיע לווטרינר שיבדוק את הכלב, ייתכן טיפול מתאים (אנטיביוטיקה לפי הצורך) ושיכוך כאבים.\n" +
+                        "במקרה הצורך יבוצעו חבישות ותימרח משחה על הכוויות.\n" +
+                        "\uFEFF", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnDogBit:
+                Toast.makeText(this, " כשכלב נושך הוא אוטומטית הופך להיות חשוד כחולה במחלת הכלבת.\n" +
+                                "כדי להגן על בריאות הציבור, יש לבודד אותו מאנשים ובעלי חיים אחרים ובאופן זה למנוע ממנו להפיץ את המחלה. \n" +
+                                "נאלץ להכניס את הכלב להסגר ביתי או עירוני על פי החלטת הויטרינר \n", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnDogTreatments:
+                Toast.makeText(this, "חיסון משושה, חיסון כלבת חיסון תולעת הפארק וטיפול נגד קרציות" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnDogNeutering:
+                Toast.makeText(this, "הדעה הוטרינרית הרווחת היא כי סירוס הוא אמצעי יעיל ונכון לשליטה על אוכלוסיית הכלבים ולשיפור חייהם וחיי הסובבים אותם.\n" +
+                        "\n לדעה זו חסרונות ויתרונות רבים ולכן חשוב להיוועץ עם הגורצמים הרלוונטים " , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnDogTrain:
+                Toast.makeText(this, "הכלב יהיה יותר ממושמע וצייתן, לא יתפתחו בעיות התנהגות, הוא יבין את מיקומו בהיררכיה, ויפתח שפה משותפת עם הבעלים.\n" +
+                        "האילוף ישפר את התקשורת בין הבעלים לכלב!\n" , Toast.LENGTH_LONG).show();
+            case R.id.btnCatVaccine:
+                Toast.makeText(this, "כל החתולים זקוקים לחיסונים ולטיפולי מניעה!\n" +
+                        "חיסון מרובע לחתולים יכלול: חתלתלת, רינוטרכיאיטיס, קליצי וכלמידיה\n" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCatWash:
+                Toast.makeText(this, " אין כל צורך ואפילו אסור לרחוץ חתולים!\n" +
+                        "מקלחת מותרת רק במקרה דחוף ובהמלצת וטרינר, אנא בלי מקלחות בבקשה - חתולים מחטאים עצמם כל הזמן" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCatMarch:
+                Toast.makeText(this, "מחלת שריטת החתול היא מחלה זיהומית הנגרמת על ידי חיידק בשם ברטונלה\n" +
+                        "החיידק מועבר לבני האדם דרך הרוק שריטה או נשיכה של החתולֿ\n" +
+                        "המחלה אינה מלווה לרוב בתסמינים חריפים או השלכות בריאותיות קשות.", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCatEat:
+                Toast.makeText(this, "כמות המזון שחתול צורך מדי יום תלויה במספר גורמים:\n" +
+                        "המשקל של החתול, רמת הפעילות של החתול וכמות הקלוריות שיש במזון.\n" +
+                        "רוב החתולים יודעים לווסת לעצמם את הכמות הדרושה וניתן פשוט לדאוג שבקערה שלהם יהיה כל הזמן מזון.\n" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCatMilk:
+                Toast.makeText(this, "לגורים מומלץ להציע אך ורק חלב אם בשבועות הראשונים לחייהם.\n " +
+                        "במידה והחתול הופרד מאמו ולא יכול לינוק, יש לתת לו אבקת חלב לחתולים, המיועדת לחתולים מגיל לידה ועד גיל 35 יום.\n" , Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCatPoop:
+                Toast.makeText(this, "יש הרבה סיבות שעלולות לגרום לחתול שלכם לא לאהוב את ארגז הצרכים \n" +
+                        "כגון: ריח לא נעים, תדירות ניקוי נמוכה, חול מסוג חדש, גודל קטן מדי של הארגז או מיקום לא נכון בבית." , Toast.LENGTH_LONG).show();
                 break;
         }
     }
