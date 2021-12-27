ae74f4a38a270a93d06f59db47fdc7ff82db53cd
kobicohen <kobicohenv1.1@gmail.com>
2021-12-27 12:12:39 +0200

    Added Vet List in Admin Screen

--- firstAPP/app/src/main/java/com/example/myfirstapp/Vet.java
+++ firstAPP/app/src/main/java/com/example/myfirstapp/Vet.java
@@ -2,15 +2,21 @@ package com.example.myfirstapp;
 
 public class Vet {
 
-
-        public String fullname,email;
-
+    public String fullname,email;
+        public Vet(){
+        }
         public Vet(String fullname,String email){
             this.fullname=fullname;
             this.email=email;
-
         }
 
-    }
+        public String getFullname() {
+            return fullname;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+}
 
 
